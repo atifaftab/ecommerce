1009726694002e1dcb569fbc581b9907b5baccdf
Atif <iamatif96@gmail.com>
2024-02-17 23:17:22 +0400

    Builder added

--- src/main/java/com/atif/ecommerce/ecommerce/entity/User.java
+++ src/main/java/com/atif/ecommerce/ecommerce/entity/User.java
@@ -3,12 +3,14 @@ package com.atif.ecommerce.ecommerce.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 @Entity
+@Builder
 public class User implements Serializable {
 
     @Id
