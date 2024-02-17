6d0870062673e93b8a4842f55814c01123ee3471
Atif <iamatif96@gmail.com>
2024-02-17 23:17:38 +0400

    product entity added

---
+++ src/main/java/com/atif/ecommerce/ecommerce/entity/Product.java
@@ -0,0 +1,21 @@
+package com.atif.ecommerce.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Builder
+public class Product implements Serializable {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String price;
+
+}
