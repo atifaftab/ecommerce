2b9c05c18619066da530d14ae4ae7f910bd0e26a
Atif <iamatif96@gmail.com>
2024-02-18 08:15:11 +0400

    common repository

--- src/main/java/com/atif/ecommerce/ecommerce/repository/CommonRepository.java
+++ src/main/java/com/atif/ecommerce/ecommerce/repository/CommonRepository.java
@@ -1,4 +1,11 @@
 package com.atif.ecommerce.ecommerce.repository;
 
-public interface CommonRepository extends JPAR{
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CommonRepository<T, ID> extends MongoRepository<T, ID> {
+    //    List<T> findAll(Example<T> example, Sort sort);
+//    Optional<T> findById(ID id);
+
+
 }
+
